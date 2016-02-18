c6c2f6866af26da5e519bc03c5293e4c7227e991
Jack Jiang <mao.jiang83@gmail.com>
2016-02-18 17:33:52 -0500

    Added sql script

--- scripts/postgresSchemas.sql
+++ scripts/postgresSchemas.sql
@@ -18,3 +18,14 @@ CREATE TABLE google_accounts
     id SERIAL PRIMARY KEY REFERENCES accounts,
     google_id TEXT UNIQUE
 );
+
+CREATE TABLE msg_history
+(
+    msg_id SERIAL,
+    offerid UUID NOT NULL,
+    userid TEXT NOT NULL,
+    type INTEGER NOT NULL,
+    sender TEXT NOT NULL,
+    message TEXT
+);
+CREATE INDEX idx_msghistory ON msg_history (offerid, userid);
